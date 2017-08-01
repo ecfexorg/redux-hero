3fed5433fe9ea6916c1b6d6967052d9208c77ded
varHarrie <varharrie@gmail.com>
2017-08-01 17:09:19 +0800

    update: support middleware

--- src/createReducer.ts
+++ src/createReducer.ts
@@ -1,32 +1,42 @@
 import * as Redux from 'redux'
 
+export type ReducerMiddleware<S> = (next: ReducerHandler<S>, state: S, payload: any, action: any) => S
+
 export type ReducerHandler<S> = (state: S, payload: any, action: any) => S
 
 export interface ReducerHandlers<S> {
   [type: string]: ReducerHandler<S>
 }
 
-function createReducer <S = any> (initialState: S, handlers: ReducerHandlers<S>): Redux.Reducer<S> {
+const defaultMiddleware: ReducerMiddleware<any> = (next, state, payload, action) => next(state, payload, action)
+const defaultHandler: ReducerHandler<any> = (state) => state
+
+function createReducer <S = any> (initialState: S, handlers: ReducerHandlers<S>): Redux.Reducer<S>
+function createReducer <S = any> (initialState: S, middleware: ReducerMiddleware<S>, handlers: ReducerHandlers<S>): Redux.Reducer<S>
+function createReducer <S = any> (...args: any[]) {
+
+  const initialState: S = args[0]
+  let middleware: ReducerMiddleware<S> = defaultMiddleware
+  let handlers: ReducerHandler<S> = args[1]
+
+  if (typeof handlers === 'function') {
+    middleware = args[1]
+    handlers = args[2]
+  }
+
   if (typeof initialState !== 'object') {
-    throw new Error('invalid initialState')
+    throw new Error('Invalid initialState')
   }
 
   if (typeof handlers !== 'object') {
-    throw new Error('invalid handlers')
+    throw new Error('Invalid handlers')
   }
 
   return (state: S = initialState, action: any = {}) => {
     const {type, payload} = action
+    let handler = (type && handlers[type]) ? handlers[type] : defaultHandler
 
-    if (type) {
-      const handler = handlers[type]
-
-      if (handler && typeof handler === 'function') {
-        return handler(state, payload, action)
-      }
-    }
-
-    return state
+    return middleware(handler, state, payload, action)
   }
 }
 
