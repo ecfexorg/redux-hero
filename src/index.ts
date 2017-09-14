ebe2b105aa0957ab8808dbc2fdcbb075cfc5a164
varHarrie <varharrie@gmail.com>
2017-09-14 16:42:30 +0800

    fix: exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import * as Redux from 'redux'
 
-export {default as createThunkMiddleware} from 'redux-thunk'
+export {default as thunk} from 'redux-thunk'
 export {default as createReducer} from './createReducer'
 
 export interface Dispatch<S = any> extends Redux.Dispatch<S> {
   <R, E>(asyncAction: ThunkAction<R, S, E>): R
 }
 
-export type ThunkAction<R = any, S = any, E = any> =
-  (dispatch: Dispatch<S>, getState: () => S, extraArgument: E) => R
+export interface ThunkAction <R = any, S = any, E = any> {
+  (dispatch: Dispatch<S>, getState: () => S, extraArgument: E): R
+}
